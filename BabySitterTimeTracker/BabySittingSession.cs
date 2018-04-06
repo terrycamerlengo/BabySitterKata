ebd9483b865c597f960d4df2c486f74b6ab83f1f
terry.camerlengo <terry.camerlengo@bluechip-llc.com>
2018-04-05 21:24:25 -0400

    init commit

---
+++ BabySitterTimeTracker/BabySittingSession.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BabySitterTimeTracker
+{
+    [Serializable()]
+    public class BabySittingSession : ISerializable
+    {
+        DateTime startTime { get; set; }
+        DateTime endTime { get; set; }
+
+        public BabySittingSession()
+        {
+            this.startTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 16, 0, 0);
+            this.endTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 4, 0, 0);
+        }
+
+        //Deserialization constructor.
+        public BabySittingSession(SerializationInfo info)
+        {
+            //Get the values from info and assign them to the appropriate properties
+            this.startTime = (DateTime)info.GetValue("startTime", typeof(DateTime));
+            this.endTime = (DateTime)info.GetValue("endTime", typeof(DateTime));
+        }
+
+        protected BabySittingSession(SerializationInfo info, StreamingContext context)
+        {
+            this.startTime = (DateTime)info.GetValue("startTime", typeof(DateTime));
+            this.endTime = (DateTime)info.GetValue("endTime", typeof(DateTime));
+        }
+
+        //Serialization function.
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("startTime", startTime);
+            info.AddValue("endTime", endTime);
+        }
+
+        public void print()
+        {
+            System.Console.WriteLine($"startTime is {this.startTime}, endTime is {this.endTime}");
+        }
+
+        public void setStartTime(int hours, int minutes)
+        {
+            this.startTime = setTime(hours, minutes);
+        }
+
+        public void setEndTime(int hours, int minutes)
+        {
+            this.endTime = setTime(hours, minutes);
+        }
+
+        public DateTime setTime(int hours, int minutes)
+        {
+            return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hours, minutes, 0);
+        }
+    }
+}
---
+++ BabySitterTimeTracker/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BabySitterTimeTracker
+{
+    class Program
+    {
+        private static BabySittingSession babySittingSession;
+        static void Main(string[] args)
+        {
+            loadBabySittingSession();
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.WriteLine("1.) List Current Session\n2.) Enter Start Time\n3.) Enter End Time\n4.) Amount Owed\n5.) Quit");
+                var command = Console.ReadLine();
+                switch (Int32.Parse(command))
+                {
+                    case (1):
+                        Program.babySittingSession.print();
+                        break;
+                    case (2):
+                        SetStartTime();
+                        break;
+                    case (3):
+                        System.Console.Write("End Time in military time hours:");
+                        var endingHours = Int32.Parse(System.Console.ReadLine());
+                        System.Console.Write("End Time in military time minutes:");
+                        var endingMinutes = Int32.Parse(System.Console.ReadLine());
+                        Program.babySittingSession.setStartTime(endingHours, endingMinutes);
+                        break;
+                    case (4):
+                        PrintAmountOwed();
+                        break;
+                    case (5):
+                        Quit();
+                        keepGoing = false;
+                        break;
+                    default:
+                        System.Console.Write("Please select a valid response. Try again.");
+                        break;
+                }
+            }
+        }
+
+        private static void SetStartTime()
+        {
+            //legit hours from 4-8 PM, or 14 thru 18
+            int startingHours = 0;
+            while (startingHours < 14 || startingHours > 18)
+            {
+                System.Console.Write("StartTime in military time hours (legit hours from 14 thru 18):");
+                startingHours = Int32.Parse(System.Console.ReadLine());
+            }
+            
+            System.Console.Write("StartTime in military time minutes:");
+            var startingMinutes = Int32.Parse(System.Console.ReadLine());
+            Program.babySittingSession.setStartTime(startingHours, startingMinutes);
+        }
+
+        private static void loadBabySittingSession()
+        {
+            //Clear mp for further usage.
+            Program.babySittingSession = null;
+
+            if (File.Exists("BabySitSession.obj"))
+            {
+                //Open the file written above and read values from it.
+                var stream = File.Open("BabySitSession.obj", FileMode.Open);
+                var bformatter = new BinaryFormatter();
+
+                Console.WriteLine("Reading Baby Sitting Information");
+                Program.babySittingSession = (BabySittingSession)bformatter.Deserialize(stream);
+                stream.Close();
+
+                Program.babySittingSession.print();
+            }
+            else
+            {
+                Program.babySittingSession = new BabySittingSession();
+            }
+        }
+
+        static void PrintAmountOwed()
+        {
+
+        }
+
+        static void Quit()
+        {
+            Stream stream = File.Open("BabySitSession.obj", FileMode.Create);
+            BinaryFormatter bformatter = new BinaryFormatter();
+
+            Console.WriteLine("Writing Babysitter Times");
+            bformatter.Serialize(stream, Program.babySittingSession);
+            stream.Close();
+        }
+    }
+}
